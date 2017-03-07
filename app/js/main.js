2b70b878e59ab1d9cd7314b7818df34f23534821
System Administrator <root@iMac-de-Alberto.local>
2017-03-07 16:48:17 -0300

    Página Index Soporte

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,38 @@ $(document).on('ready', function () {
     var margenTop = $('.hero').css('margin-top', valorAlto);
     // console.log(margenTop);
 });
+
+
+
+//-- Padding Nav
+$(document).on('ready', function () {
+ var navHeight = $('.navbar').height();
+ var paddingNav = (navHeight - 20) / 2  + 'px';
+  $('.nav.navbar-nav.navbar-right li > a').css({
+      'padding-top': paddingNav,
+      'padding-bottom': paddingNav,
+    });
+    console.log(paddingNav);
+});
+
+//-- JQuery Easing
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+        $('.navbar-collapse.navbar-ex1-collapse.collapse').removeClass('in').attr('aria-expanded','false');
+    });
+});
