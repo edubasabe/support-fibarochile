2e96d94407ab3fcb4a22510b8f012d069a3622cf
edubasabe <edubasabe1@gmail.com>
2017-05-11 14:29:06 -0300

    resellers expertos

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ gulp.task('useref', function(){
   return gulp.src('app/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
+    // .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'));
 });
 
